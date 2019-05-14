ddae0083bfc739adcadac11d87c396729b20424b
Alina Ghetler <alina.ghetler@gmail.com>
2019-05-14 10:47:41 +0200

    adding mongoose models

--- src/models/Order.js
+++ src/models/Order.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
+ date :{type :Date, default: Date.now },
+ order:{type: String, required: true},
+ deliveryAddress:{type: String, required: true}
+ 
     
 })
 
