7e4b4b313530abad17b6d0bca22a33c6c15b2114
Alina Ghetler <alina.ghetler@gmail.com>
2019-05-14 19:27:36 +0200

    change auth

--- src/app.js
+++ src/app.js
@@ -2,33 +2,38 @@
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const passport = require("passport");
+const mongoConnectURI = process.env.MONGOURL || "mongodb://localhost/fruits-delivery";
 
-const fs = require('fs')
-const http = require('http')
-const WebSocket = require('ws')
-
+const fs = require("fs");
+const http = require("http");
+const WebSocket = require("ws");
+const bodyParser = require("body-parser");
+const user = require("./routes/user");
+const auth = require("./routes/auth");
+require("./auth/passport");
 
 const HERE_APP_ID = "sTWYdO0PrgRXmMm1ViBr";
 const HERE_APP_CODE = "IdqCe27szfQfJ9i4z5Zq6Q";
+mongoose
+    .connect(mongoConnectURI, { useNewUrlParser: true })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .catch(err => {
+        console.error("Error connecting to mongo", err);
+    });
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
-    // if you use Model.id as your idAttribute maybe you'd want
-    // done(null, user.id);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
-app.use(passport.initialize());
-
+app.use("/api", user);
+app.use("/auth", auth);
 app.get("/", (req, res) => res.json({ greeting: "Hello World!" }));
 
 // generate certificates: openssl req -nodes -new -x509 -keyout server.key -out server.cert
 const httpServer = http.createServer(app);
-
 
 const wss = new WebSocket.Server({
     server: httpServer
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,98 +1,25 @@
-const express = require("express");
+const express = require('express');
+const router  = express.Router();
+const jwt = require('jsonwebtoken');
 const passport = require("passport");
-const router = express.Router();
-
-// Bcrypt to encrypt passwords
-const bcrypt = require("bcrypt");
-const bcryptSalt = 10;
-const User = require("../models/User");
-const LocalStrategy = require("passport-local").Strategy;
-
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-  // if you use Model.id as your idAttribute maybe you'd want
-  // done(null, user.id);
+/* POST login. */
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local', {session: false}, (err, user, info) => {
+        if (err || !user) {
+            return res.status(400).json({
+                message: 'Something is not right',
+                user   : user
+            });
+        }
+       req.login(user, {session: false}, (err) => {
+           if (err) {
+               res.send(err);
+           }
+           // generate a signed son web token with the contents of user object and return it in the response
+           const token = jwt.sign(user.toJSON(), 'your_jwt_secret');
+           return res.json({user, token});
+        });
+    })(req, res);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(
-  new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    });
-  })
-);
-
-router.get("/login", (req, res, next) => {
-  res.render("login");
-});
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/registration",
-    failureRedirect: "/login",
-    failureFlash: true,
-    passReqToCallback: true
-  })
-);
-
-router.get("/signup", (req, res, next) => {
-  res.render("signup");
-});
-
-router.post("/signup", (req, res, next) => {
-  const { username, password, email } = req.body;
-  if (username === "" || password === "" || email === "") {
-    res.render("signup", { message: "Indicate username and password" });
-    return;
-  }
-
-  User.findOne({ username }, "username", (err, user) => {
-    console.log(user);
-    if (user !== null) {
-      res.render("signup", { message: "The username already exists" });
-      return;
-    }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username,
-      email,
-      password: hashPass
-    });
-
-    newUser
-      .save()
-      .then(user => {
-        res.redirect("/login");
-      })
-      .catch(err => {
-        console.error("Error saving new user", err);
-        res.render("signup", { message: "Something went wrong" });
-      });
-  });
-});
-
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
-});
-
-module.exports = router;
+module.exports = router
