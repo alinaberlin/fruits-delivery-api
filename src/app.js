1d009a3cc13f44d43febdbeb6a269055597ccb64
Alina Ghetler <alina.ghetler@gmail.com>
2019-05-14 08:15:46 +0200

    Hello json

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const app = express()
-const passport = require('passport');
+const express = require("express");
+const app = express();
+const passport = require("passport");
 
-const fs = require('fs')
-const https = require('https')
-const WebSocket = require('ws')
+const fs = require("fs");
+const https = require("https");
+const WebSocket = require("ws");
 
-const HERE_APP_ID = 'sTWYdO0PrgRXmMm1ViBr'
-const HERE_APP_CODE = 'IdqCe27szfQfJ9i4z5Zq6Q'
+const HERE_APP_ID = "sTWYdO0PrgRXmMm1ViBr";
+const HERE_APP_CODE = "IdqCe27szfQfJ9i4z5Zq6Q";
 
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -23,19 +23,21 @@ passport.deserializeUser(function(user, done) {
 
 app.use(passport.initialize());
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get("/", (req, res) => res.json({ greeting: "Hello World!" }));
 
 // generate certificates: openssl req -nodes -new -x509 -keyout server.key -out server.cert
-const httpServer = https.createServer({
-    key: fs.readFileSync('ssl/server.key'),
-    cert: fs.readFileSync('ssl/server.cert')
-}, app);
+const httpServer = https.createServer(
+    {
+        key: fs.readFileSync("ssl/server.key"),
+        cert: fs.readFileSync("ssl/server.cert")
+    },
+    app
+);
 
 const wss = new WebSocket.Server({
-    'server': httpServer
+    server: httpServer
 });
 
 module.exports = {
     httpServer
-}
+};
