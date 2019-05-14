a1dec0dc7dcc8cef64d8de30785a33f052ff9f21
Alina Ghetler <alina.ghetler@gmail.com>
2019-05-14 12:25:07 +0200

    Token generation

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const app = express();
-const passport = require("passport");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const fs = require('fs')
@@ -15,35 +14,8 @@ const userRoutes = require('./routes/user-routes')
 const HERE_APP_ID = "sTWYdO0PrgRXmMm1ViBr";
 const HERE_APP_CODE = "IdqCe27szfQfJ9i4z5Zq6Q";
 // auth settings
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('./models/User')
-const opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
-passport.serializeUser(function(user, done) {
-    done(null, user);
-    // if you use Model.id as your idAttribute maybe you'd want
-    // done(null, user.id);
-});
-
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
-        console.log("This is called")
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
-    });
-}));
+require('./auth/passport');
+const auth = require('./routes/auth');
 // initialize mongo connection
 mongoose
     .connect(mongoConnectURI, { useNewUrlParser: true })
@@ -58,24 +30,11 @@ mongoose
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(passport.initialize());
+
 app.use('/api', userRoutes)
+app.use('/auth', auth);
 app.get("/", (req, res) => res.json({ greeting: "Hello World!" }));
 
-/*app.post(
-    "/api/login",
-    (req, res) => {
-        res.json(req.body)
-    }
-  );*/
-app.post(
-    "/api/login",
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        console.log("Authenticate")
-        res.json(req.user.profile);
-    }
-  );
 // generate certificates: openssl req -nodes -new -x509 -keyout server.key -out server.cert
 const httpServer = http.createServer(app);
 
